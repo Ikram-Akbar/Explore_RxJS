f846ab5729307c1185e4a9e862c73b2cc2782608
Ikram Akbar <ikramakbar71@gmail.com>
2022-09-26 12:54:08 +0600

    added subject

---
+++ src/concateMap/concateMap.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rxjs_1 = require("rxjs");
+const source = (0, rxjs_1.of)(2000, 1000);
---
+++ src/concateMap/concateMap.ts
@@ -0,0 +1,4 @@
+
+import { of } from "rxjs";
+const source = of(2000, 1000);
+
